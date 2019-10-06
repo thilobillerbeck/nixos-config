882703cc4ebc9d5d351541ebb2c800cce77563de
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2019-10-06 22:53:01 +0200

    added workstation basic

---
+++ machines/workstation/configuration.nix
@@ -0,0 +1,138 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  system.autoUpgrade.enable = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "thilo-pc"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "de";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    ddate
+    file
+    htop
+    manpages
+    tmux
+    tree
+    wget
+    vim
+    neovim
+    zsh
+    chromium
+    vscode
+    git
+    gnumake
+    gcc
+    linuxHeaders
+    spotify
+    numix-gtk-theme
+    numix-icon-theme-circle
+    gnupg
+    curl
+    unzip
+    imagemagick
+    youtube-dl
+    go
+    openjdk
+    nodejs
+    watchman
+    vagrant
+    kubectl
+    steam
+    python
+    discord
+  ];
+
+  environment.etc."xdg/gtk-3.0/settings.ini" = {
+    text = ''
+      [Settings]
+      gtk-icon-theme-name=Numix-circle
+      gtk-theme-name=Numix
+      gtk-application-prefer-dark-theme = true
+    '';
+  };
+
+  programs.zsh.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "de";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.thilo = {
+    isNormalUser = true;
+    extraGroups = [ "wheel"  "docker" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03";
+
+  services.xserver.videoDrivers = [ "amdgpu" ];                                                                                                                                                                                                                                                      
+  hardware.cpu.amd.updateMicrocode = true;                                                                                                                                   
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+
+  virtualisation.docker.enable = true;
+
+  security.sudo.wheelNeedsPassword = false;
+}
+
