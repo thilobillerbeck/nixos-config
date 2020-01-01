4007f54073656ec2222271b79281bffcaa0e5187
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2020-01-01 18:50:06 +0100

    added public key to thilo

--- users/thilo.nix
+++ users/thilo.nix
@@ -8,6 +8,7 @@
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9TwM1zgEQiU8Cl0OszpU/fba4NpG2rjNSoTvvm/Vcf thilo@thilo-pc"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGHXL+1Q6MeNJoqaC4IlUXBIhLiRPzyM2Je11rQrXsiD"
     ];
     extraGroups = [
       "dialout"
