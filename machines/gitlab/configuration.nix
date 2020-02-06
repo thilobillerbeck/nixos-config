8f7ef13db98cfba97e9b0f426139397b8163828d
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2020-02-06 23:24:14 +0100

    updated gitlab

--- machines/gitlab/configuration.nix
+++ machines/gitlab/configuration.nix
@@ -58,7 +58,7 @@ in {
         "${registry_url}" = {
           enableACME = true;
           forceSSL = true;
-          locations."/".proxyPass = "http://localhost:${local_registry_port}";
+          locations."/".proxyPass = "http://127.0.0.1:5000";
         };
       };
     };
