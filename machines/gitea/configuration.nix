dee2c2fbbffa834c2e2364cb58b3c6c267a25d97
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2020-10-26 14:15:43 +0100

    „machines/gitea/configuration.nix“ ändern

--- machines/gitea/configuration.nix
+++ machines/gitea/configuration.nix
@@ -73,11 +73,20 @@ in {
         	        APP_NAME = Thilos SCM
 
         	        [service]
-                 DISABLE_REGISTRATION = true
+                 	DISABLE_REGISTRATION = true
 
         	        [ui]
-                  DEFAULT_THEME = arc-green
+                  	DEFAULT_THEME = arc-green
         	        SHOW_USER_EMAIL = false
+
+					[mailer]
+                    ENABLED        = true
+                    FROM           = git@officerent.de
+                    MAILER_TYPE    = smtp
+                    HOST           = mail.officerent.de:587
+                    IS_TLS_ENABLED = true
+                    USER           = git@officerent.de
+                    PASSWD         = `cogypost91`
         	      '';
     };
     postgresql = {
