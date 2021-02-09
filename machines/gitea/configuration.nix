531458760940c5b1dddf740fa31ccadd05e05959
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2021-02-09 23:44:16 +0100

    updated drone config

--- machines/gitea/configuration.nix
+++ machines/gitea/configuration.nix
@@ -26,6 +26,50 @@ in {
     firewall.allowedTCPPorts = [ 22 80 443 ];
   };
 
+    systemd.services.drone-server = {
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        EnvironmentFile = [ ];
+        Environment = [
+          "DRONE_GITEA=true"
+          "DRONE_GITEA_SERVER=https://${gitea_url}";
+          "DRONE_GITEA_CLIENT_ID=146c9b07-8c10-40c3-8cf4-e391258a6768";
+          "DRONE_GITEA_CLIENT_SECRET=_48BPPhEFm-OJlbJRbXoKM1swcs_PStXJlKOUPPsuiU=";
+          "DRONE_RPC_SECRET=65e33f4b929df4e4efcb00859e504e8d";
+          "DRONE_SERVER_HOST=ci.thilo-billerbeck.com";
+          "DRONE_SERVER_PORT=:4000"
+          "DRONE_SERVER_PROTO=https";
+          "DRONE_USER_CREATE=username:thilobillerbeck,admin:true";
+        ];
+        ExecStart = "${pkgs.drone}/bin/drone-server";
+        User = thilo;
+        Group = thilo;
+      };
+    };
+
+  systemd.services.drone-agent = {
+    wantedBy = [ "multi-user.target" ];
+    # might break deployment
+    restartIfChanged = true;
+    serviceConfig = {
+      Environment = [
+        "DRONE_RPC_SECRET=65e33f4b929df4e4efcb00859e504e8d"
+        "DRONE_RPC_HOST=ci.thilo-billerbeck.com"
+        "DRONE_RPC_PROTO=https"
+        "DRONE_SERBER_PORT=4000"
+        "DRONE_SERVER=https://ci.thilo-billerbeck.com"
+        "DRONE_RUNNER_CAPACITY=2"
+        "DRONE_NAME=runner"
+      ];
+      EnvironmentFile = [ ];
+      ExecStart = "${pkgs.drone}/bin/drone-agent";
+      User = "drone-agent";
+      Group = "drone-agent";
+      SupplementaryGroups = [ "docker" ];
+      DynamicUser = true;
+    };
+  };
+
   virtualisation.oci-containers.containers = {
     "drone" = {
       image = "drone/drone:1";
@@ -40,7 +84,7 @@ in {
         "DRONE_USER_CREATE"="username:thilobillerbeck,admin:true";
       };
       volumes = [ "/var/lib/drone:/data" ];
-      ports = [ "4000:80" "4001:443" ];
+      # ports = [ "4000:80" "4001:443" ];
     };
     "drone-runner" = {
       image = "drone/drone-runner-docker:1";
@@ -51,9 +95,16 @@ in {
         "DRONE_RUNNER_CAPACITY" = "4";
       };
       volumes = [ "/var/run/docker.sock:/var/run/docker.sock" ];
-      ports = [ "3000:3000" ];
+      # ports = [ "3000:3000" ];
     };
   };
+
+  users.users.droneserver = {
+    isSystemUser = true;
+    createHome = true;
+    group = droneserver;
+  };
+  users.groups.droneserver = {};
 
   time.timeZone = "Europe/Berlin";
 
@@ -124,6 +175,7 @@ in {
       identMap = # Map the gitea user to postgresql
         ''
           gitea-users gitea gitea
+
         '';
     };
   };
