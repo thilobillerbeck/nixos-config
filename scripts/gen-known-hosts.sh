bd48eab4dc917f8b29a1b2879e39b2412893e2e7
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-05-15 16:42:52 +0200

    added remote deployment

---
+++ scripts/gen-known-hosts.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ssh-keyscan -t ed25519 -H bart.thilo-billerbeck.com
+ssh-keyscan -t ed25519 -H burns.thilo-billerbeck.com
+ssh-keyscan -t ed25519 -H krusty.thilo-billerbeck.com
+ssh-keyscan -t ed25519 -H lisa.thilo-billerbeck.com
--- users/root.nix
+++ users/root.nix
@@ -2,6 +2,8 @@
   users.users.root = {
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys =
-      config.users.users.thilo.openssh.authorizedKeys.keys;
+      config.users.users.thilo.openssh.authorizedKeys.keys ++ [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcjUNTzZ2lZTDFzCrWynnZAu+47IwI/WXmNBRAT9lZZ d"
+      ];
   };
 }
