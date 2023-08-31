14272dd80cec20e35ac3f56433f6737cd62c64bc
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-08-31 18:08:38 +0200

    updated obsidian sync

--- packages/rev-obsidian-sync.nix
+++ packages/rev-obsidian-sync.nix
@@ -5,22 +5,22 @@
 
 buildGoModule rec {
   pname = "rev-obsidian-sync";
-  version = "0.0.1.1";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "acheong08";
     repo = "rev-obsidian-sync";
     rev = "v${version}";
-    hash = "sha256-8mo3NfKie+CcvzeJcMla3eDOS+ogoXTs9+PovgArhpI=";
+    hash = "sha256-sVaq04Pe8aCXMboLX3XrY0qhSBk3PKIEWZtvLsiDHW8=";
   };
 
-  vendorHash = "sha256-uMBWFEHA4FURKpOBaCCd1bPqbRcqNS0rXoCiW8lT2EY=";
+  vendorHash = "sha256-A/WQ9GCGiA9rncGI+zTy/iqmaXsOa4TIU7XS9r6wMnQ=";
 
   ldflags = [ "-s" "-w" ];
 
   meta = with lib; {
     description = "Reverse engineering of the native Obsidian sync and publish server";
-    homepage = "https://github.com/acheong08/rev-obsidian-sync";
+    homepage = "https://github.com/acheong08/rev-obsidian-sync/";
     license = licenses.gpl2Only;
     maintainers = with maintainers; [ ];
   };
