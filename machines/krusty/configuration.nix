bd54fb1c8b335f898a8e9c4bbcca5e2ff51c6f39
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-09-29 00:20:26 +0200

    modularized invoiceninja

--- machines/krusty/configuration.nix
+++ machines/krusty/configuration.nix
@@ -1,15 +1,7 @@
 { config, pkgs, lib, ... }:
 let
   invoiceninja_app = "invoiceninja";
-  invoiceninja_domain = "invoiceninja.thilo-billerbeck.com";
-  invoiceninja_dataDir = "/srv/http/${invoiceninja_domain}";
   sources = import ./../../nix/sources.nix;
-  invoiceninja_php = pkgs.php.buildEnv {
-    extensions = { enabled, all }: with all; enabled ++ [ memcached ];
-    extraConfig = ''
-      memory_limit = 512M
-    '';
-  };
   unstable = import sources.unstable { config.allowUnfree = true; };
 in
 {
@@ -17,6 +9,7 @@ in
     ./../../configs/server.nix
     ./hardware.nix
     ./../../modules/colmena-upgrade.nix
+    ./../../modules/invoiceninja.nix
     ./../../modules/containers/watchtower.nix
     ./../../users/deploy.nix
   ];
@@ -69,61 +62,10 @@ in
     firewall = { allowedTCPPorts = [ 22 80 443 9001 8055 ]; };
   };
 
-  systemd.timers."invoiceninja" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnBootSec = "1m";
-      OnUnitActiveSec = "1m";
-      Unit = "invoiceninja.service";
-    };
-  };
-
-  systemd.services."invoiceninja" = {
-    script = ''
-      cd /srv/http/${invoiceninja_domain}
-      ${invoiceninja_php}/bin/php artisan schedule:run
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = config.services.nginx.user;
-    };
-  };
-  systemd.services.invoiceninja-scheduler = {
-    description = "invoiceninja scheduler";
-    startAt = "minutely";
-
-    unitConfig = {
-      ConditionPathExists = "${invoiceninja_dataDir}/.env";
-      ConditionDirectoryNotEmpty = "${invoiceninja_dataDir}/vendor";
-    };
-
-    serviceConfig = {
-      Type = "oneshot";
-      User = config.services.nginx.user;
-      Group = config.services.nginx.user;
-      SyslogIdentifier = "invoiceninja-scheduler";
-      WorkingDirectory = "${invoiceninja_dataDir}";
-      ExecStart = "${invoiceninja_php}/bin/php artisan schedule:run -v";
-    };
-  };
-
   services = {
-    phpfpm.pools.${invoiceninja_app} = {
-      user = config.services.nginx.user;
-      phpPackage = invoiceninja_php;
-      settings = {
-        "listen.owner" = config.services.nginx.user;
-        "pm" = "dynamic";
-        "pm.max_children" = 32;
-        "pm.max_requests" = 500;
-        "pm.start_servers" = 2;
-        "pm.min_spare_servers" = 2;
-        "pm.max_spare_servers" = 5;
-        "php_admin_value[error_log]" = "stderr";
-        "php_admin_flag[log_errors]" = true;
-        "catch_workers_output" = true;
-      };
-      phpEnv."PATH" = lib.makeBinPath [ invoiceninja_php pkgs.mysql ];
+    invoiceninja = {
+      enable = true;
+      domain = "invoiceninja.thilo-billerbeck.com";
     };
     nginx = {
       enable = true;
@@ -142,46 +84,6 @@ in
           forceSSL = true;
           locations."/".proxyPass = "http://localhost:8056";
         };
-        "${invoiceninja_domain}" = {
-          enableACME = true;
-          forceSSL = true;
-          extraConfig = ''
-            server_name ${invoiceninja_domain};
-            root ${invoiceninja_dataDir}/public;
-            index index.php index.html index.htm;
-            client_max_body_size 20M;
-
-            gzip on;
-            gzip_types      application/javascript application/x-javascript text/javascript text/plain application/xml application/json;
-            gzip_proxied    no-cache no-store private expired auth;
-            gzip_min_length 1000;
-
-            location / {
-                try_files $uri $uri/ =404;
-            }
-
-            location ~* \.pdf$ {
-                add_header Cache-Control no-store;
-            }
-
-            if (!-e $request_filename) {
-                rewrite ^(.+)$ /index.php?q= last;
-            }
-
-            location ~* /storage/.*\.php$ {
-                return 503;
-            }
-
-            location ~ \.php$ {
-              include ${pkgs.nginx}/conf/fastcgi.conf;
-              fastcgi_pass unix:${config.services.phpfpm.pools.${invoiceninja_app}.socket};
-            }
-
-            location ~ /\.ht {
-                deny all;
-            }
-          '';
-        };
         "trilium.thilo-billerbeck.com" = {
           enableACME = true;
           forceSSL = true;
@@ -193,10 +95,5 @@ in
       };
     };
     trilium-server.enable = true;
-    mysql = {
-      enable = true;
-      dataDir = "/var/lib/mariadb";
-      package = pkgs.mariadb;
-    };
   };
 }
