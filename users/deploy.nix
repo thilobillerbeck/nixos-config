afec39835e2fa0819d5abccc21623aee35adfcad
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-11-25 01:19:07 +0100

    make deploy normal user

--- users/deploy.nix
+++ users/deploy.nix
@@ -6,7 +6,7 @@
     uid = 1001;
     description = "Deployment User";
     shell = pkgs.zsh;
-    isSystemUser = true;
+    isNormalUser = true;
     openssh.authorizedKeys.keys = [
       # deploy
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIl8SUvg7bNgODg9JgfFYLAu1yDF6q2gQDeeEqvDHqsG"
