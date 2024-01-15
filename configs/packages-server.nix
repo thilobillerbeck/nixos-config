04ebaf699c72b5c4eb59ebd894a0939422166e83
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-01-15 22:38:56 +0100

    add kernel-check based reboot

--- configs/packages-server.nix
+++ configs/packages-server.nix
@@ -15,5 +15,18 @@
     curl
     unzip
     ncdu
+    (pkgs.writeShellScriptBin "kernel-check-reboot" ''
+      currentKernel=$(readlink /run/current-system/kernel)
+      bootedKernel=$(readlink /run/booted-system/kernel)
+      echo "Current Kernel Path:" $currentKernel
+      echo "Booted Kernel Path:" $bootedKernel
+
+      if [[ "$currentKernel" != "$bootedKernel" ]]; then
+          echo "Kernel Versions differ. Rebooting...."
+          systemctl reboot -i
+      else
+          echo "Current kernel is booted. Skipping...."
+      fi
+    '')
   ];
 }
